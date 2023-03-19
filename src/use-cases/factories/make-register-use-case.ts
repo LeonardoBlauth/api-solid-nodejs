77674a351ff075a7f0c158d5ca3589bc77fb1d24
Leonardo Blauth <leonardo.blauth@dealerup.com.br>
2023-03-19 09:23:12 -0300

    🐛 Fix import

--- src/use-cases/factories/make-register-use-case.ts
+++ src/use-cases/factories/make-register-use-case.ts
@@ -1,4 +1,4 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/PrismaUsersRepository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { RegisterUseCase } from '../register'
 
 export function makeRegisterUseCase() {
