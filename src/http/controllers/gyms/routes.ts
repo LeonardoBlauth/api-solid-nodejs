a8a7914f8b5b1644fe31986982411758ef50a1ab
Leonardo Blauth <leonardo.blauth@dealerup.com.br>
2023-03-19 21:51:04 -0300

    📦 Create check-ins controllers

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
 import { create } from './create'
 import { nearby } from './nearby'
 import { search } from './search'
